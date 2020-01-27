6e267ec352da6294013c5f26abde9adc8e22e100
scypher6 <github email address>
2020-01-27 16:36:53 -0500

    adding scypher6 as username

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 export default class ColorBox extends Component {
 
   render() {
+    // console.log(ColorBox)
     if (this.props.opacity >= 0.2)
         return (
-                <div className="color-box" style={{opacity: this.props.opacity}}>
+          <div className="color-box" style={{opacity: this.props.opacity}}>
+
                   <ColorBox opacity={this.props.opacity - 0.1} />
                 </div>
     )
