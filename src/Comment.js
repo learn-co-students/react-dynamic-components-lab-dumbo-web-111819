f00726eb7c8a7dc063160bb58586ab9ec57b8b59
ajshemi <akpojotor@yahoo.com>
2020-01-27 18:04:11 -0500

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,16 @@
 //your code here
+
+import React, { Component } from 'react';
+// import BlogPost from './BlogPost'
+
+export default class Comment extends Component {
+    render(){
+        console.log(this.props.commentText)
+        return(<div className="comment">
+            {this.props.commentText}
+        </div>
+        );
+    }
+}
+
+//Comment is the child of BlogPost can access all its attributes/props given from its parent using this.props.'name_of_attribute'
